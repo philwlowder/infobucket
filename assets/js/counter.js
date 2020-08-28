2f90d478807bac60f97d46c5bba0d4f8cde0672d
philwlowder <lowder.phil@gmail.com>
2020-08-28 19:01:27 -0400

    added statcounter code

--- assets/js/counter.js
+++ assets/js/counter.js
@@ -1,15 +1,30 @@
-< !--Default Statcounter code for My personal website https://infobucket.com -->
-  <script type="text/javascript">
-    var sc_project=12254161;
-    var sc_invisible=1;
-    var sc_security="c340161b";
-    var sc_https=1;
+// Default Statcounter code for My personal website https://infobucket.com 
+<script type="text/javascript">
+  var sc_project=12254161;
+  var sc_invisible=1;
+  var sc_security="c340161b";
+  var sc_https=1;
 </script>
-    <script type="text/javascript"
-      src="https://www.statcounter.com/counter/counter.js" async></script>
-    <noscript><div class="statcounter"><a title="Web Analytics"
-      href="https://statcounter.com/" target="_blank"><img class="statcounter"
-        src="https://c.statcounter.com/12254161/0/c340161b/1/" alt="Web
+  <script type="text/javascript"
+    src="https://www.statcounter.com/counter/counter.js" async></script>
+  <noscript><div class="statcounter"><a title="Web Analytics"
+    href="https://statcounter.com/" target="_blank"><img class="statcounter"
+      src="https://c.statcounter.com/12254161/0/c340161b/1/" alt="Web
 Analytics"></a></div></noscript>
-      <!-- End of Statcounter Code -->
-<a href="https://statcounter.com/p12254161/?guest=1">View My Stats</a>
+
+    <!-- End of Statcounter Code -->
+    <a href="https://statcounter.com/p12254161/?guest=1">View My Stats</a>
+
+
+
+    <script type='text/javascript'>
+      var sc_project=12254161;
+      var sc_invisible=1;
+      var sc_security="c340161b";
+      var sc_https=1;
+      var scJsHost = (("https:" == document.location.protocol) ?
+      "https://secure." : "http://www.");
+document.write("<sc"+"ript type='text/javascript' async src='" +
+scJsHost+
+"statcounter.com/counter/counter_xhtml.js'></"+"script>");
+</script>

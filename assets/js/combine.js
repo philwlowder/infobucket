0d5648b462382d28fc14bfd90ae58acb1f54e398
philwlowder <lowder.phil@gmail.com>
2020-10-16 15:49:43 -0400

    updated nav menu javascript

--- assets/js/combine.js
+++ assets/js/combine.js
@@ -19,5 +19,22 @@ window.onload = init;
 
 /* nav.js for menu */
 
-!function (n) { n(function () { n("nav ul li > a:not(:only-child)").click(function (i) { n(this).siblings(".nav-dropdown").toggle(), n(".nav-dropdown").not(n(this).siblings()).hide(), i.stopPropagation() }), n("html").click(function () { n(".nav-dropdown").hide() }) }), document.querySelector("#nav-toggle").addEventListener("click", function () { this.classList.toggle("active") }), n("#nav-toggle").click(function () { n("nav ul").toggle() }) }(jQuery);
 
+(function($) {
+  $(function() {
+    $('nav ul li > a:not(:only-child)').click(function(e) {
+      $(this).siblings('.nav-dropdown').toggle();
+      $('.nav-dropdown').not($(this).siblings()).hide();
+      e.stopPropagation();
+    });
+    $('html').click(function() {
+      $('.nav-dropdown').hide();
+    });
+  });
+  document.querySelector('#nav-toggle').addEventListener('click', function() {
+    this.classList.toggle('active');
+  });
+  $('#nav-toggle').click(function() {
+    $('nav ul').toggle();
+  });
+})(jQuery);
